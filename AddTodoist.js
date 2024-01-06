7cad0da53238e8a6f165a78f2eebb41d812e1bf9
Andrew Lee <andrewlhy@gmail.com>
2024-01-06 13:41:23 -0700

    Add highlighter for functionality

--- AddTodoist.js
+++ AddTodoist.js
@@ -16,12 +16,24 @@ GM_registerMenuCommand ("Add Item To Do Ist Task List", copyData, "T");
 function copyData () {
     var todoist_content = prompt('Add Task Name');
     var todoist_description = prompt('Add Task Description');
+    const outlinedNotes = document.getElementsByClassName("highlighter--highlighted");
+    var stringData = "";
+    var i = 0
+
+    for(i=0; i < outlinedNotes.length ; i++){
+        if(i > 0){
+            stringData += "\n";
+        }
+        if(outlinedNotes[i].innerHTML.valueOf().length){
+            stringData += outlinedNotes[i].innerHTML.valueOf();
+        }
+    }
         GM_xmlhttpRequest ( {
             method:     "POST",
             url:        "https://api.todoist.com/rest/v2/tasks",
             data:       JSON.stringify({
                 'content': todoist_content,
-                'description': todoist_description
+                'description': todoist_description + " \n\n " + stringData
             }),
             headers:    {
                 'accept': 'application/json',
